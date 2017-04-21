a5c51222b8262868eeb8e735cb6766ae55c09cfe
heartsucker <heartsucker@autistici.org>
2017-04-21 10:43:52 +0200

    rsassa-pss test vector

--- generator.py
+++ generator.py
@@ -14,19 +14,32 @@ import binascii
 import ed25519
 import hashlib
 import json
+import logging
 import os
 
 from canonicaljson import encode_canonical_json as cjson
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_PSS
 
 
 OUTPUT_DIR = path.join(path.dirname(path.abspath(__file__)), 'vectors')
+
+log = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
+log.setLevel(logging.DEBUG)
 
 
 def main():
     vector_meta = []
     for repo in Repo.subclasses():
+        log.info('Generating repo {}'.format(repo.NAME))
         repo = repo()
         vector_meta.append(repo.vector_meta())
+        log.info('Repo {} done'.format(repo.NAME))
 
     # verify vector_meta
     for meta in vector_meta:
@@ -57,6 +70,8 @@ def key_id(pub):
 def key_type(sig_method):
     if sig_method == 'ed25519':
         return 'ed25519'
+    elif sig_method == 'rsassa-pss-sha256':
+        return 'rsa'
     else:
         raise Exception('unknown signature method: {}'.format(sig_method))
 
@@ -80,16 +95,20 @@ def sign(keys, signed):
         if sig_method == 'ed25519':
             priv = ed25519.SigningKey(binascii.unhexlify(priv))
             sig = priv.sign(data, encoding='hex').decode('utf-8')
-
-            sig_data = {
-                'keyid': key_id(pub),
-                'method': sig_method,
-                'sig': sig,
-            }
-
-            sigs.append(sig_data)
+        elif sig_method == 'rsassa-pss-sha256':
+            h = SHA256.new(data)
+            rsa = RSA.importKey(priv)
+            signer = PKCS1_PSS.new(rsa)
+            sig = binascii.hexlify(signer.sign(h)).decode('utf-8')
         else:
             raise Exception('unknown signature method: {}'.format(sig_method))
+
+        sig_data = {
+            'keyid': key_id(pub),
+            'method': sig_method,
+            'sig': sig,
+        }
+        sigs.append(sig_data)
 
     return sigs
 
@@ -109,45 +128,63 @@ class Repo:
         self.root_keys = []
 
         for version, keys in enumerate(self.ROOT_KEYS):
+            log.info('Making keys for root version {}'.format(version + 1))
+
             root_key_group = []
 
             for key_num, sig_method in enumerate(keys):
-                key_data = self.gen_key('{}.root-{}'.format(version + 1, key_num + 1),
-                                    sig_method)
-                root_key_group.append((sig_method, key_data[0], key_data[1]))
+                log.info('Making root key {} with method {}'.format(key_num + 1, sig_method))
+
+                priv, pub = self.gen_key('{}.root-{}'.format(version + 1, key_num + 1),
+                                         sig_method)
+                root_key_group.append((sig_method, priv, pub))
 
             self.root_keys.append(root_key_group)
 
         self.targets_keys = []
-        for sig_method in self.TARGETS_KEYS:
-            key_data = self.gen_key('targets', sig_method)
-            self.targets_keys.append((sig_method, key_data[0], key_data[1]))
+        for key_num, sig_method in enumerate(self.TARGETS_KEYS):
+            log.info('Making targets key {} with method {}'.format(key_num + 1, sig_method))
+
+            priv, pub = self.gen_key('targets-{}'.format(key_num + 1), sig_method)
+            self.targets_keys.append((sig_method, priv, pub))
 
         self.timestamp_keys = []
-        for sig_method in self.TIMESTAMP_KEYS:
-            key_data = self.gen_key('timestamp', sig_method)
-            self.timestamp_keys.append((sig_method, key_data[0], key_data[1]))
+        for key_num, sig_method in enumerate(self.TIMESTAMP_KEYS):
+            log.info('Making timestamp key {} with method {}'.format(key_num + 1, sig_method))
+
+            priv, pub = self.gen_key('timestamp-{}'.format(key_num + 1), sig_method)
+            self.timestamp_keys.append((sig_method, priv, pub))
 
         self.snapshot_keys = []
-        for sig_method in self.SNAPSHOT_KEYS:
-            key_data = self.gen_key('snapshot', sig_method)
-            self.snapshot_keys.append((sig_method, key_data[0], key_data[1]))
+        for key_num, sig_method in enumerate(self.SNAPSHOT_KEYS):
+            log.info('Making snapshot key {} with method {}'.format(key_num + 1, sig_method))
+
+            priv, pub = self.gen_key('snapshot-{}'.format(key_num + 1), sig_method)
+            self.snapshot_keys.append((sig_method, priv, pub))
 
         for target, content in self.TARGETS:
+            log.info('Writing target: {}'.format(target))
+
             with open(path.join(self.output_dir, 'targets', target), 'wb') as f:
                 f.write(self.alter_target(content))
 
         self.make_root(1)
         for version, root in enumerate(self.root_meta):
+            log.info('Making root metadata version {}'.format(version))
+
+            # TODO cross sign verion N+1 with keys from version N
             self.write_meta('{}.root'.format(version + 1), root)
         self.write_meta('root', self.root_meta[-1])
 
+        log.info('Making targets metadata')
         self.make_targets()
         self.write_meta('targets', self.targets_meta)
 
+        log.info('Making snapshot metadata')
         self.make_snapshot()
         self.write_meta('snapshot', self.snapshot_meta)
 
+        log.info('Making timestamp metadata')
         self.make_timestamp()
         self.write_meta('timestamp', self.timestamp_meta)
 
@@ -159,27 +196,33 @@ class Repo:
         return path.join(OUTPUT_DIR, self.NAME)
 
     def gen_key(self, role, sig_method):
-        if sig_method == 'ed25519':
-            try:
-                with open(path.join(self.output_dir, 'keys', '{}.priv'.format(role)), 'r') as f:
-                    priv = f.read().strip()
+        typ = key_type(sig_method)
 
-                with open(path.join(self.output_dir, 'keys', '{}.pub'.format(role)), 'r') as f:
-                    pub = f.read().strip()
-            except FileNotFoundError:
+        try:
+            with open(path.join(self.output_dir, 'keys', '{}.priv'.format(role)), 'r') as f:
+                priv = f.read().strip()
+
+            with open(path.join(self.output_dir, 'keys', '{}.pub'.format(role)), 'r') as f:
+                pub = f.read().strip()
+        except FileNotFoundError:
+            if typ == 'ed25519':
                 priv, pub = ed25519.create_keypair()
                 priv = binascii.hexlify(priv.to_bytes()).decode('utf-8')
                 pub = binascii.hexlify(pub.to_bytes()).decode('utf-8')
-            finally:
-                with open(path.join(self.output_dir, 'keys', '{}.priv'.format(role)), 'w') as f:
-                    f.write(priv)
-                    f.write('\n')
+            elif typ == 'rsa':
+                rsa = RSA.generate(2048)
+                priv = rsa.exportKey(format='PEM').decode('utf-8')
+                pub = rsa.publickey().exportKey(format='PEM').decode('utf-8')
+            else:
+                raise Exception('unknown key type: {}'.format(typ))
+        finally:
+            with open(path.join(self.output_dir, 'keys', '{}.priv'.format(role)), 'w') as f:
+                f.write(priv)
+                f.write('\n')
 
-                with open(path.join(self.output_dir, 'keys', '{}.pub'.format(role)), 'w') as f:
-                    f.write(pub)
-                    f.write('\n')
-        else:
-            raise Exception('unknown signature method: {}'.format(sig_method))
+            with open(path.join(self.output_dir, 'keys', '{}.pub'.format(role)), 'w') as f:
+                f.write(pub)
+                f.write('\n')
 
         return (priv, pub)
 
@@ -225,10 +268,10 @@ class Repo:
         for sig_method, pub in keys:
             signed['keys'][key_id(pub)] = {
                 'keytype': key_type(sig_method),
-                'keyvalue': { 'public': pub },
+                'keyvalue': {'public': pub},
             }
 
-        meta = {'signatures': sign(self.root_keys[version - 1], signed), 'signed': signed }
+        meta = {'signatures': sign(self.root_keys[version - 1], signed), 'signed': signed}
 
         if not hasattr(self, 'root_meta'):
             self.root_meta = []
@@ -254,7 +297,7 @@ class Repo:
             'targets': file_data,
         }
 
-        self.targets_meta = {'signatures': sign(self.targets_keys, signed), 'signed': signed }
+        self.targets_meta = {'signatures': sign(self.targets_keys, signed), 'signed': signed}
 
     def make_snapshot(self):
         signed = {
@@ -271,19 +314,19 @@ class Repo:
         # TODO not sure if all versions of root need to be included
         for version, root in enumerate(self.root_meta):
             name = '{}.root.json'.format(version + 1)
-
+            jsn = jsonify(root)
             signed['meta'][name] = {
-                'length': len(root),
+                'length': len(jsn),
                 'version': root['signed']['version'],
                 'hashes': {
-                    'sha512': sha512(jsonify(root).encode('utf-8')),
-                    'sha256': sha256(jsonify(root).encode('utf-8')),
+                    'sha512': sha512(jsn.encode('utf-8')),
+                    'sha256': sha256(jsn.encode('utf-8')),
                 },
             }
 
             signed['meta']['root.json'] = signed['meta'][name]
 
-        self.snapshot_meta = {'signatures': sign(self.snapshot_keys, signed), 'signed': signed }
+        self.snapshot_meta = {'signatures': sign(self.snapshot_keys, signed), 'signed': signed}
 
     def make_timestamp(self):
         signed = {
@@ -302,7 +345,7 @@ class Repo:
             }
         }
 
-        self.timestamp_meta = {'signatures': sign(self.timestamp_keys, signed), 'signed': signed }
+        self.timestamp_meta = {'signatures': sign(self.timestamp_keys, signed), 'signed': signed}
 
     @classmethod
     def vector_meta(cls):
@@ -341,6 +384,17 @@ class Repo002(Repo):
         new = bytearray(target)
         new[0] ^= 0x01
         return bytes(new)
+
+
+class Repo003(Repo):
+
+    NAME = '003'
+    IS_SUCCESS = True
+
+    ROOT_KEYS = [['rsassa-pss-sha256']]
+    TARGETS_KEYS = ['rsassa-pss-sha256']
+    TIMESTAMP_KEYS = ['rsassa-pss-sha256']
+    SNAPSHOT_KEYS = ['rsassa-pss-sha256']
 
 
 if __name__ == '__main__':
