e2ba8a357b71f82d893c3eb8a815f466dee87f80
heartsucker <heartsucker@autistici.org>
2017-06-26 17:24:59 +0200

    server fixes, readme

--- server.py
+++ server.py
@@ -8,18 +8,42 @@ from os import path
 from tuf_vectors.server import init_app
 
 
-def main(repo_type, vector_dir, port):
-    app = init_app(repo_type, vector_dir)
+def main(
+        repo_type,
+        port,
+        key_type,
+        signature_scheme,
+        signature_encoding,
+        compact,
+        cjson_strategy):
+    app = init_app(repo_type, key_type=key_type, signature_scheme=signature_scheme,
+            signature_encoding=signature_encoding, compact=compact, cjson_strategy=cjson_strategy)
     app.run(host='127.0.0.1', port=port, debug=True)
 
 if __name__ == '__main__':
     parser = ArgumentParser(path.basename(__file__),
                             description='Runs a TUF repo HTTP server')
-    parser.add_argument('-p', '--path', help='The path to serve content from.', required=True)
     parser.add_argument('-P', '--port', help='The port to bind the app to',
                         type=int, default=8080)
     parser.add_argument('-t', '--type', help='The type of repo to serve',
                         default='tuf', choices=['tuf', 'uptane'])
+    parser.add_argument('--signature-encoding', help='The encoding for cryptographic signatures',
+                        default='hex', choices=['hex', 'base64'])
+    parser.add_argument('--compact', help='Write JSON in compact format', action='store_true')
+    parser.add_argument('--cjson', help='The formatter to use for canonical JSON',
+                        default='olpc', choices=['olpc', 'json-subset'])
+    parser.add_argument('--key-type', help='The key type to use',
+                        default='ed25519', choices=['ed25519', 'rsa-2048', 'rsa-4096', 'rsa-8192'])
+    parser.add_argument(
+        '--signature-scheme',
+        help='The signature scheme to use',
+        default='ed25519',
+        choices=[
+            'ed25519',
+            'rsassa-pss-sha256',
+            'rsassa-pss-sha512'])
     args = parser.parse_args()
 
-    main(args.type, path.abspath(args.path), args.port)
+    main(args.type, args.port,
+         args.key_type, args.signature_scheme,
+         args.signature_encoding, args.compact, args.cjson)
--- tuf_vectors/server.py
+++ tuf_vectors/server.py
@@ -2,79 +2,99 @@
 
 import json
 
-from flask import Flask, send_from_directory, Response, abort
+from flask import Flask, Response, abort, make_response
+from functools import wraps
 from os import path
 from tuf_vectors import subclasses
 from tuf_vectors.tuf import Tuf
 from tuf_vectors.uptane import Uptane
 
 
-def init_app(repo_type, vector_dir):
-    print(vector_dir)
+def json_response(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        out = f(*args, **kwargs)
+        resp = make_response(json.dumps(out, separators=(',', ':')))
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
+    return decorated_function
+
+def init_app(
+        repo_type,
+        key_type,
+        signature_scheme,
+        signature_encoding,
+        compact,
+        cjson_strategy):
     app = Flask(__name__, static_folder=None, template_folder=None)
 
     repos = {}
     counter = {}
 
     for repo in subclasses(Tuf if repo_type == 'tuf' else Uptane):
+        repo = repo('/tmp/', key_type=key_type, signature_scheme=signature_scheme,
+                    signature_encoding=signature_encoding,
+                    compact=compact, cjson_strategy=cjson_strategy)
         repos[repo.name()] = repo
 
     @app.route('/')
+    @json_response
     def index():
-        out = json.dumps(list(repos.keys()), separators=(',', ':'))
-        return Response(response=out, headers={'Content-Type': 'application/json'})
+        return list(repos.keys())
 
     @app.route('/<string:repo>/step', methods=['POST'])
+    @json_response
     def step(repo):
         current = counter.get(repo, 0)
         try:
-            r = repos[repo]
-            try:
-                step_meta = r.STEPS[current].generate_meta()
-            except IndexError:
-                abort(400)
+            step_meta = repos[repo].steps[current].generate_meta()
         except KeyError:
-            abort(404)
+            abort(400)
+        except IndexError:
+            return '', 204
 
+        counter[repo] = current + 1
         # TODO if current step == 0, include root keys for pinning
-        out = {
+        return {
             'update': step_meta['update'],
             'targets': step_meta['targets'],
         }
-
-        out = json.dumps(out, separators=(',', ':'))
-        counter[repo] = current + 1
-        return Response(response=out, headers={'Content-Type': 'application/json'})
 
     @app.route('/<string:repo>/reset', methods=['POST'])
     def reset(repo):
         counter.pop(repo, None)
         return '', 204
 
-    @app.route('/<string:repo>/<int:root_version>.root.json')
-    def root(repo, root_version):
-        current = counter.get(repo)
-        if current is None:
-            abort(400)
+    if repo_type == 'tuf':
+        @app.route('/<string:repo>/<int:root_version>.root.json')
+        def root(repo, root_version):
+            current = counter.get(repo)
+            if current is None:
+                abort(400)
 
-        root_idx = root_version - 1
-        if current >= root_idx:
-            return send_from_directory(vector_dir,
-                                       path.join(repo,
-                                                 str(root_idx),
-                                                 'root.json'))
-        else:
-            return abort(404)
+            root_idx = root_version - 1
+            if current >= root_idx:
+                try:
+                    return repos[repo].steps[root_idx].root
+                except (IndexError, ValueError):
+                    abort(400)
+            else:
+                return abort(404)
 
-    @app.route('/<string:repo>/<path:content_path>')
-    def repo(repo, content_path):
-        current = counter.get(repo)
-        if current is None:
-            abort(400)
+        @app.route('/<string:repo>/<path:content_path>')
+        def repo(repo, content_path):
+            try:
+                current = counter[repo]
+                repo = repos[repo].steps[current - 1]
+            except (IndexError, KeyError) as e:
+                abort(400)
 
-        return send_from_directory(vector_dir,
-                                   path.join(repo,
-                                             str(current),
-                                             *content_path.split('/')))
+            for target in repo.TARGETS:
+                if target[0] == content_path:
+                    return target[1]
+
+            abort(404)
+    else:
+        pass # TODO
 
     return app
