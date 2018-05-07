ae0d0a7ae92fd06918742b98391f0f282d3f60bd
heartsucker <heartsucker@autistici.org>
2018-05-07 11:13:32 +0200

    cleaner test parametrization

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -1,28 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import sys
+import pytest
 
 from tempfile import TemporaryDirectory
 from tuf_vectors import subclasses
 from tuf_vectors.uptane import Uptane
 
 
-for sub in subclasses(Uptane):
-    def gen_test():
-        def test_it(self):
-            with TemporaryDirectory(prefix='tuf-test-vectors') as tempdir:
-                sub = self.CLS(output_dir=tempdir,
-                               key_type='ed25519',
-                               signature_scheme='ed25519',
-                               signature_encoding='hex',
-                               compact=True,
-                               cjson_strategy='olpc',
-                               ecu_identifier='123',
-                               hardware_id='abc')
-                sub.persist()
-        return test_it
-
-    name = 'Test' + sub.__name__
-    cls = type(name, (object,), {'test_it': gen_test(),
-                                 'CLS': sub})
-    setattr(sys.modules[__name__], name, cls)
+@pytest.mark.parametrize("cls", subclasses(Uptane))
+def test_basic(cls):
+    with TemporaryDirectory(prefix='tuf-test-vectors') as tempdir:
+        sub = cls(output_dir=tempdir,
+                  key_type='ed25519',
+                  signature_scheme='ed25519',
+                  signature_encoding='hex',
+                  compact=True,
+                  cjson_strategy='olpc',
+                  ecu_identifier='123',
+                  hardware_id='abc')
+        sub.persist()
