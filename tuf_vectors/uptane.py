7c56758d86e579053bf0e818c530a9c5f778bdc6
heartsucker <heartsucker@autistici.org>
2018-05-16 14:48:20 +0200

    bad ecu id tests

--- tuf_vectors/uptane.py
+++ tuf_vectors/uptane.py
@@ -2819,3 +2819,62 @@ class BadHwIdUptane(Uptane):
     STEPS = [
         (DirectorStep, ImageStep),
     ]
+
+
+class DirectorBadEcuIdUptane(Uptane):
+
+    '''The director targets metadata has a bad ECU ID'''
+
+    class ImageStep(Step):
+
+        TARGETS_KEYS_IDX = [1]
+        SNAPSHOT_KEYS_IDX = [2]
+        TIMESTAMP_KEYS_IDX = [3]
+
+        ROOT_KWARGS = {
+            'root_keys_idx': [0],
+            'targets_keys_idx': TARGETS_KEYS_IDX,
+            'snapshot_keys_idx': SNAPSHOT_KEYS_IDX,
+            'timestamp_keys_idx': TIMESTAMP_KEYS_IDX,
+        }
+
+        TARGETS_KWARGS = {
+            'targets_keys_idx': TARGETS_KEYS_IDX,
+        }
+
+        SNAPSHOT_KWARGS = {
+            'snapshot_keys_idx': SNAPSHOT_KEYS_IDX,
+        }
+
+        TIMESTAMP_KWARGS = {
+            'timestamp_keys_idx': TIMESTAMP_KEYS_IDX,
+        }
+
+    class DirectorStep(Step):
+
+        TARGET_ERRORS = {
+            DEFAULT_TARGET_NAME: 'BadEcuId',
+        }
+
+        TARGETS_KEYS_IDX = [5]
+
+        ROOT_KWARGS = {
+            'root_keys_idx': [4],
+            'targets_keys_idx': TARGETS_KEYS_IDX,
+        }
+
+        def __targets(hardware_id: str, ecu_identifier: str=None) -> list:
+            return [Target(name=DEFAULT_TARGET_NAME,
+                           content=DEFAULT_TARGET_CONTENT,
+                           hardware_id=hardware_id,
+                           ecu_identifier=ecu_identifier,
+                           alteration='bad-ecu-id')]
+
+        TARGETS_KWARGS = {
+            'targets_keys_idx': TARGETS_KEYS_IDX,
+            'targets': __targets,
+        }
+
+    STEPS = [
+        (DirectorStep, ImageStep),
+    ]
